98ceee809f72475672082c69377b023fb33afa7d
José Farley Sousa da Silva <jose.farley@academico.ifpb.edu.br>
2023-02-06 13:25:34 -0300

    Definindo a estrutura da guia de consulta

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express')
+const xml2js = require('xml2js')
+const fs = require('fs')
 const app = express()
 app.use(express.json())
 app.use(express.static('Padrão TISS Comunicação 040100/'))
 app.get('/', (req, res)=>{
     res.send('main route')
 })
+app.post('/consultaGuia/create/', async (req, res)=>{
+
+})
+
 app.listen(8080, ()=>console.log('server runing...'))
+
+let consultaGuia = fs.readFile(__dirname +'/consulta.xml', async (err, data)=>{
+    if(err) throw new Error(err)
+    const parser = new xml2js.Parser()
+    let a = await parser.parseStringPromise(data)
+    console.log(a['ans:mensagemTISS']['ans:cabecalho'][0]['ans:destino'][0]['ans:identificacaoPrestador'][0])
+})
